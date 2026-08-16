83459d489f118355b398b94b330c8b55f2b0c686
gabriele-ps <gabrieleps.pereira@gmail.com>
2026-08-16 09:47:31 -0400

    fix: arruma importações

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@ from src.leitura import ler_json, ler_csv, ler_txt
 from src.validacao import validar_campos_obrigatorios, validar_email, validar_registro, validar_tempo, extrair_protocolos, extrair_telefones
 from src.processamento import remover_duplicados, remover_espacos, tratar_dados, tratar_valores_ausentes, mapear_categorias, padronizar_datas, padronizar_textos
 from src.analise import quantidade_total_atendimentos, quantidade_por_categoria, quantidade_por_status, tempo_medio_atendimento, categoria_mais_frequente, percentual_invalidos
-from scr.relatorios import grafico_por_categoria, grafico_distribuicao_tempo, grafico_por_status, exportar_csv, exportar_resumo_json
+from src.relatorios import grafico_por_categoria, grafico_distribuicao_tempo, grafico_por_status, exportar_csv, exportar_resumo_json
 
 __version__ = '1.0.0'
